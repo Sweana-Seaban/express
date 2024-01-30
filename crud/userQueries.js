494a9ea579eaa3821e7d2e53aff9886b368ba865
sweana <sweana@bititude.com>
2024-01-30 15:27:28 +0530

    Exclude fields,where clause,limit,ordering and grouping

--- crud/userQueries.js
+++ crud/userQueries.js
@@ -35,9 +35,36 @@ User.sync({alter:true}).then(() => {
     //return User.findAll();
 
     //select only specific fields
-    return User.findAll({attributes:['username','password']})
-}).then((data) => {
+    //return User.findAll({attributes:['username','password']})
 
+    //select using aliases
+    //return User.findAll({attributes: [['username','myName'],['password','passwords']]});
+
+    //aggregate
+    //return User.findAll({attributes : [[sequelize.fn('SUM',sequelize.col('age')),'total_age']]});
+
+    //excluding fields
+    //return User.findAll({attributes: {exclude:['password']}});
+
+    //where clause
+    //return  User.findAll({attributes: ['username'],where:{age:23}});
+
+    //multiple where clause
+    //return User.findAll({where : {age:23,username:'rose'} })
+
+    //limit result particular number of rows
+    //return User.findAll({limit:2})
+
+    //ordering
+    //return User.findAll({order :[['age','desc']]});
+
+    //grouping
+    return User.findAll({
+        attributes:['username',
+    [sequelize.fn('sum',sequelize.col('age')),'sum_age']],
+    group:'username'
+    });
+}).then((data) => {
     data.forEach(element => {
         console.log(element.toJSON());
     });
