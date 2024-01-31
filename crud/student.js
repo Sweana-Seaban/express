78c330ca7c853391097fcd6094426ad4ddfd7325
sweana <sweana@bititude.com>
2024-01-31 11:15:22 +0530

    Bulk create records

--- crud/student.js
+++ crud/student.js
@@ -26,20 +26,59 @@ const Student = sequelize.define('student',{
     },
     favourite_class:{
         type:DataTypes.STRING,
-        default:'Computer Science'
+        defaultValue:'Computer Science'
     },
     school_year:{
         type:DataTypes.INTEGER,
         allowNull:false
     },
     subscribed_to_wittcode:{
-        type:DataTypes.TINYINT,
-        default:true
+        type:DataTypes.BOOLEAN,
+        defaultValue:true
     }
 })
 
 Student.sync({alter:true}).then(() => {
     console.log('Users table created successfully');
+    Student.bulkCreate([
+        {
+            name:'isabella',
+            school_year:2000
+        },
+        {
+            name:'edward',
+            school_year:2001
+        },
+        {
+            name:'charlie',
+            favourite_class:'design',
+            school_year:2002
+        },
+        {
+            name:'jacob',
+            school_year:2000
+        },
+        {
+            name:'carlisle',
+            favourite_class:'medicine',
+            school_year:2001,
+            subscribed_to_wittcode:false
+        },
+        {
+            name:'rene',
+            school_year:2000
+        },
+        {
+            name:'esme',
+            favourite_class:'design',
+            school_year:2002,
+            subscribed_to_wittcode:false
+        },
+        {
+            name:'charlotte',
+            school_year:2000
+        }
+    ],{validate:true});
 }).catch(() => {
     console.log('Error in table creation');
 })
